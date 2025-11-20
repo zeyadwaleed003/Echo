e5bf7d239b0a50750a97e85f2ed40deb220d7cf1
zeyadwaleed003 <zeyadw526@gmail.com>
2025-11-20 15:26:38 +0200

    docs(bookmarks): add swagger docs to bookmarks

--- src/modules/bookmarks/bookmarks.controller.ts
+++ src/modules/bookmarks/bookmarks.controller.ts
@@ -20,13 +20,23 @@ import { RolesGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { Role } from '../accounts/accounts.enums';
 import type { QueryString } from 'src/common/types/api.types';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Bookmarks')
+@ApiBearerAuth()
 @Controller('bookmarks')
 @UseGuards(AuthGuard)
 export class BookmarksController {
   constructor(private readonly bookmarksService: BookmarksService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a bookmark' })
   create(@Body() createBookmarkDto: CreateBookmarkDto, @Req() req: Request) {
     return this.bookmarksService.create(
       req.account!.id,
@@ -37,22 +47,33 @@ export class BookmarksController {
   @Get()
   @UseGuards(RolesGuard)
   @Roles(Role.ADMIN)
+  @ApiBody({ type: CreateBookmarkDto })
+  @ApiQuery({ name: 'page', required: false })
+  @ApiQuery({ name: 'limit', required: false })
+  @ApiQuery({ name: 'sort', required: false })
+  @ApiOperation({ summary: 'Get all bookmarks (Admin only)' })
   findAll(@Query() q: QueryString) {
     return this.bookmarksService.findAll(q);
   }
 
   @Get('me')
+  @ApiQuery({ name: 'page', required: false })
+  @ApiQuery({ name: 'limit', required: false })
+  @ApiQuery({ name: 'sort', required: false })
+  @ApiOperation({ summary: 'Get current user bookmarks' })
   findCurrentUserBookmarks(@Query() q: QueryString, @Req() req: Request) {
     return this.bookmarksService.findCurrentUserBookmarks(req.account!.id, q);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a bookmark by id' })
   findOne(@Param() idDto: IdDto, @Req() req: Request) {
     return this.bookmarksService.findOne(req.account!.id, idDto.id);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'Delete a bookmark' })
   remove(@Param() idDto: IdDto, @Req() req: Request) {
     return this.bookmarksService.remove(req.account!.id, idDto.id);
   }
--- src/modules/bookmarks/dto/create-bookmark.dto.ts
+++ src/modules/bookmarks/dto/create-bookmark.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class CreateBookmarkDto {
+  @ApiProperty({
+    description: 'ID of the post to bookmark',
+    example: 123,
+    type: Number,
+  })
   @IsNumber()
   postId: number;
 }

995f326d8ca04922b40af7d951760d569a367ff5
zeyadwaleed003 <zeyadw526@gmail.com>
2025-11-21 14:57:39 +0200

    feat(search): implement searching through recent posts
    Note: Needs more performance, logic enhancement

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -3,9 +3,11 @@ import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from '../accounts/entities/account.entity';
+import { Post } from '../posts/entities/post.entity';
+import { PostFiles } from '../posts/entities/post-file.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account, Post, PostFiles])],
   controllers: [SearchController],
   providers: [SearchService],
 })
--- src/modules/search/search.service.ts
+++ src/modules/search/search.service.ts
@@ -5,12 +5,18 @@ import { SearchFilter } from './search.enums';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Account } from '../accounts/entities/account.entity';
 import { ILike, LessThanOrEqual, Repository } from 'typeorm';
+import { Post } from '../posts/entities/post.entity';
+import { PostFiles } from '../posts/entities/post-file.entity';
 
 @Injectable()
 export class SearchService {
   constructor(
     @InjectRepository(Account)
-    private readonly accountRepository: Repository<Account>
+    private readonly accountRepository: Repository<Account>,
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+    @InjectRepository(PostFiles)
+    private readonly postFilesRepository: Repository<PostFiles>
   ) {}
 
   private decodeCursor(cursor: string | undefined) {
@@ -34,6 +40,17 @@ export class SearchService {
     return Buffer.from(id.toString(), 'utf-8').toString('base64');
   }
 
+  private assignNextCursor<T extends { id: number }>(
+    result: T[],
+    resultLength: number,
+    limit: number
+  ) {
+    if (resultLength > limit)
+      return this.encodeCursor(result[resultLength - 1]!.id);
+
+    return null;
+  }
+
   // This works perfectly fine
   private async searchAccounts(
     id: number | undefined,
@@ -55,15 +72,58 @@ export class SearchService {
       take: limit + 1,
     });
 
-    let nextCursor: string | null = null;
-    if (accounts.length > limit)
-      nextCursor = this.encodeCursor(accounts[accounts.length - 1]!.id);
+    // Assign next cursor
+    const nextCursor = this.assignNextCursor<Account>(
+      accounts,
+      accounts.length,
+      limit
+    );
 
     const paginatedAccounts = accounts.slice(0, limit);
 
     return {
       size: paginatedAccounts.length,
       data: paginatedAccounts,
+      nextCursor,
+    };
+  }
+
+  private async searchLatestPosts(
+    id: number | undefined,
+    q: string,
+    limit: number
+  ) {
+    // Get matching posts
+    const posts = await this.postRepository.find({
+      where: {
+        content: ILike(`%${q}%`),
+        ...(id ? { id: LessThanOrEqual(id) } : {}),
+      },
+      relations: ['account', 'actionPost'],
+      order: { createdAt: 'DESC' },
+      take: limit + 1,
+    });
+
+    // Assign next cursor
+    const nextCursor = this.assignNextCursor<Post>(posts, posts.length, limit);
+
+    // Paginated posts is the limited number of posts without the extra post
+    const paginatedPosts = posts.slice(0, limit);
+
+    // Get and assign the files of each post
+    const postsFilesPromises = paginatedPosts.map(
+      async (p) => await this.postFilesRepository.findBy({ postId: p.id })
+    );
+    const postsFiles = await Promise.all(postsFilesPromises);
+
+    const postsWithFiles = paginatedPosts.map((p, i) => ({
+      ...p,
+      postFiles: postsFiles[i],
+    }));
+
+    return {
+      size: postsWithFiles.length,
+      data: postsWithFiles,
       nextCursor,
     };
   }
@@ -78,6 +138,9 @@ export class SearchService {
       case SearchFilter.USER:
         // Search in account table to find any matching accounts
         return this.searchAccounts(id, q, limit);
+      case SearchFilter.LIVE:
+        // Search through the latest posts
+        return this.searchLatestPosts(id, q, limit);
     }
 
     return {};
